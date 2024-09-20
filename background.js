7d5d87d414c74de4921fa10673d8c22ece7d0727
Alvin Nguyen <106125101+alvinwin@users.noreply.github.com>
2024-09-20 12:14:16 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // background.js
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Text Finder extension installed');
+  console.log('PromptNavi extension installed');
   chrome.storage.sync.set({enabled: false}, function() {
     console.log('Extension disabled by default');
   });
@@ -9,7 +9,7 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'findText') {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'findText', text: request.text });
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'findText', text: request.text, caseSensitive: request.caseSensitive });
     });
   }
 });
